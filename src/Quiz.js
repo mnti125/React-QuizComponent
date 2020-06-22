7e4351e32d61f231156505f1ea20eedf9f3a1283
Michael Nti <michaelnti@Michaels-MBP-2.home>
2020-06-21 22:43:46 -0400

    Part 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,22 +1,38 @@
-import React, {Component} from 'react'
-import QuizQuestion from './QuizQuestion.js'
+import React, { Component } from "react";
+import QuizQuestion from "./QuizQuestion.js";
+import QuizEnd from "./QuizEnd.js";
 
-let quizData = require('./quiz_data.json');
+let quizData = require("./quiz_data.json");
 
 class Quiz extends Component {
-    constructor(props){
-        super(props);
-        //setting the state of the quiz component
-        this.state = {
-            quiz_position: 1
-        }
-    }
+  constructor(props) {
+    super(props);
+    //setting the state of the quiz component
+    this.state = {
+      quiz_position: 1,
+    };
+  }
 
-    render(){
-        return(
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-        )
-    }
+  render() {
+      const isQuizEnd = false;
+    const display = (isQuizEnd) ? <QuizEnd /> : <QuizQuestion
+    quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />;
+    return (
+      <div>
+          
+          
+             {/* <QuizEnd />
+          
+             <QuizQuestion 
+             quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
+           />
+           */}
+         {display}
+        
+       
+      </div>
+    );
+  }
 }
 
-export default Quiz
+export default Quiz;
